c1c48c98593f082a06cc17eaa2db8189f3f808bc
mandeephanda <89677278+mandeepsna@users.noreply.github.com>
2023-08-10 14:39:16 -0400

    Update Taskmanager.js

--- components/Taskmanager.js
+++ components/Taskmanager.js
@@ -1,10 +1,12 @@
 "use client";
 import React from "react";
+
 import { useState, useEffect } from "react";
 import Styles from "../src/app/style.module.css";
 import axios from "axios";
 const Taskmanager = () => {
   let [products, setProduct] = useState([]);
+  const value;
   useEffect(() => {
     axios.get("http://localhost:3000/api/employee").then((response) => {
       setProduct(response.data);
